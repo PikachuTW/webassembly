5ff3e62218005a6bb71a7fc8c7b74e79a6a0f05f
PikachuTW <82265690+PikachuTW@users.noreply.github.com>
2025-06-10 22:00:37 +0800

    final

--- playground/compile.sh
+++ playground/compile.sh
@@ -4,5 +4,4 @@ emcc main.c -o main.wasm \
     -O3 \
     -ffast-math \
     --no-entry \
-    -s EXPORTED_FUNCTIONS='["_factorial","_isPrime","_countPrimes","_fibonacciRecursive","_matrixMultiplication"]' \
     -s ALLOW_MEMORY_GROWTH=1 \
